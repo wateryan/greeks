0f983e816633d9e763928735bf387749477045ed
Ryan <wateryan280@gmail.com>
2017-03-14 21:40:46 -0500

    added gamma calculation

--- src/greeks.rs
+++ src/greeks.rs
@@ -6,6 +6,7 @@
 /// q - continously compounded dividend yield (%)
 /// t - time to expiration (% of year)
 use std::f64::consts::E;
+use std::f64::consts::PI;
 use stats::cnd;
 
 pub fn delta_call(s0: f64, x: f64, t: f64, r: f64, q: f64, sigma: f64) -> f64 {
@@ -20,6 +21,14 @@ pub fn delta_put(s0: f64, x: f64, t: f64, r: f64, q: f64, sigma: f64) -> f64 {
     let cnd = cnd(d1);
     let e = E.powf(-(q * t));
     return e * (cnd - 1.0);
+}
+
+pub fn gamma(s0: f64, x: f64, t: f64, r: f64, q: f64, sigma: f64) -> f64 {
+    let d1 = d1(s0, x, t, r, q, sigma);
+    let arg1 = E.powf(-(q * t)) / (s0 * sigma * (t.sqrt()));
+    let arg2 = 1.0 / ((2.0 * PI).sqrt());
+    let arg3 = E.powf((-d1).powf(2.0)) / 2.0;
+    return arg1 * arg2 * arg3;
 }
 
 pub fn d1(s0: f64, x: f64, t: f64, r: f64, q: f64, sigma: f64) -> f64 {
@@ -38,63 +47,78 @@ mod tests {
 
     use greeks::*;
 
-    const UNDERLYING: f64 = 36.07;
-    const STRIKE: f64 = 35.00;
-    const VOL: f64 = 0.4825;
-    const INTEREST_RATE: f64 = 0.01;
-    const DIV_YIELD: f64 = 0.00;
-    const TIME_TO_EXPIRY: f64 = 26.0 / 365.0;
+    const UNDERLYING: f64 = 64.68;
+    const STRIKE: f64 = 65.00;
+    const VOL: f64 = 0.5051;
+    const INTEREST_RATE: f64 = 0.0150;
+    const DIV_YIELD: f64 = 0.0210;
+    const TIME_TO_EXPIRY: f64 = 23.0 / 365.0;
+
+    const E_D1: f64 = -0.1053;
+    const E_D2: f64 = 0.02149;
+    const E_CALL_DELTA: f64 = 0.5079;
+    const E_PUT_DELTA: f64 = -0.4908;
+    const E_GAMMA: f64 = 0.0486;
 
     #[test]
     fn test_d1() {
-        let expected_d1: f64 = 0.3038;
         let d1 = d1(UNDERLYING,
                     STRIKE,
                     TIME_TO_EXPIRY,
                     INTEREST_RATE,
                     DIV_YIELD,
                     VOL);
-        let abs = (d1 - expected_d1).abs();
+        println!("{}", d1);
+        let abs = (d1 - E_D1).abs();
         assert!(abs < 0.001);
     }
 
     #[test]
     fn test_d2() {
-        let expected_d2 = 0.1751;
         let d2 = d2(UNDERLYING,
                     STRIKE,
                     TIME_TO_EXPIRY,
                     INTEREST_RATE,
                     DIV_YIELD,
                     VOL);
-        let abs = (d2 - expected_d2).abs();
+        let abs = (d2 - E_D2).abs();
         assert!(abs < 0.001);
     }
 
     #[test]
     fn test_delta_call() {
-        let expected_delta = 0.6194;
         let call_delta = delta_call(UNDERLYING,
                                     STRIKE,
                                     TIME_TO_EXPIRY,
                                     INTEREST_RATE,
                                     DIV_YIELD,
                                     VOL);
-        let abs = (call_delta - expected_delta).abs();
+        let abs = (call_delta - E_CALL_DELTA).abs();
         assert!(abs < 0.001);
     }
 
     #[test]
     fn test_delta_put() {
-        let expected_delta = -0.3806;
         let put_delta = delta_put(UNDERLYING,
                                   STRIKE,
                                   TIME_TO_EXPIRY,
                                   INTEREST_RATE,
                                   DIV_YIELD,
                                   VOL);
-        let abs = (put_delta - expected_delta).abs();
-        println!("{}", put_delta);
+        let abs = (put_delta - E_PUT_DELTA).abs();
+        assert!(abs < 0.001);
+    }
+
+    #[test]
+    fn test_gamma() {
+        let gamma = gamma(UNDERLYING,
+                          STRIKE,
+                          TIME_TO_EXPIRY,
+                          INTEREST_RATE,
+                          DIV_YIELD,
+                          VOL);
+        let abs = (gamma - E_GAMMA).abs();
+        println!("{}", gamma);
         assert!(abs < 0.001);
     }
 }
